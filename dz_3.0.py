4fe89f446a3b30d974536b366f2b710ec9d6466c
aamigas <alexandr.migas@gmail.com>
2021-07-31 10:23:27 +0300

    + Счетчик внутри функции

--- dz_3.0.py
+++ dz_3.0.py
@@ -8,7 +8,7 @@ for i in range(N):
         F.append(i)
     else:
         F.append(F[i-1] + F[i-2])
-    s += 1
+        s += 1
 print(F)
 print('Количество вызовов операции сложения без рекурсии равно ', s)
    
@@ -18,14 +18,14 @@ def fibon(n):
   if n <= 1:
       return n    
   else:
-      return(fibon(n-1) + fibon(n-2)) 
+      global m; m += 1
+      return (fibon(n-1) + fibon(n-2)) 
 
-s = 0
 F = []
-for i in range(N):
+m = 0
+for i in range(int(N)):
     F.append(fibon(i))
-    s += 1
 print(F)
-print('Количество вызовов операции сложения с рекурсией равно ', s)
+print('Количество вызовов операции сложения с рекурсией равно ', m)
 
 
