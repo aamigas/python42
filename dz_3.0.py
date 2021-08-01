de7f433d002dae3524315547fb43dc3aff7eabe3
aamigas <alexandr.migas@gmail.com>
2021-08-01 07:54:37 +0300

    + debugger

--- dz_3.0.py
+++ dz_3.0.py
@@ -1,18 +1,23 @@
 """Вывести список чисел Фибоначчи (F) от 1 до N с помощью цикла"""
 N = int(input('Введите число ', ))
 
-s = 0
-F = []
-for i in range(N):
-    if i <= 1:
-        F.append(i)
-    else:
-        F.append(F[i-1] + F[i-2])
-        s += 1
-print(F)
-print('Количество вызовов операции сложения без рекурсии равно ', s)
+# s = 0
+# F = []
+# for i in range(N):
+#     if i <= 1:
+#         F.append(i)
+#     else:
+#         F.append(F[i-1] + F[i-2])
+#         s += 1
+# print(F)
+# print('Количество вызовов операции сложения без рекурсии равно ', s)
    
 """Вывести список чисел Фибоначчи (F) от 1 до N через рекурсию"""
+
+import pdb
+
+F = []
+m = 0
 
 def fibon(n):
   if n <= 1:
@@ -21,10 +26,10 @@ def fibon(n):
       global m; m += 1
       return (fibon(n-1) + fibon(n-2)) 
 
-F = []
-m = 0
-for i in range(int(N)):
+for i in range(N):
+    pdb.set_trace()
     F.append(fibon(i))
+    print(m)
 print(F)
 print('Количество вызовов операции сложения с рекурсией равно ', m)
 
